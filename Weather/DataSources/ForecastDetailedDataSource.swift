80fdee1feeb26110513833c3769575638f0ad6a8
GitFraize <zabrovsky2000denis@gmail.com>
2021-09-22 17:03:33 +0300

    Update forecast detailed

--- Weather/DataSources/ForecastDetailedDataSource.swift
+++ Weather/DataSources/ForecastDetailedDataSource.swift
@@ -1,9 +1,13 @@
 import UIKit
 
-struct DayForecastDetailedDataSource {
+struct ForecastDetailedDayDataSource {
     let temp: String
     let feelsLike: String
     let icon: UIImage
     let date: String
     let description: String
+    let forecast: [ForecastHourDataSource]
+    let wind: String
+    let humidity: String
+    let precipitation: String
 }
