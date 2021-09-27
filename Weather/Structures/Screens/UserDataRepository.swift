234de72abfe9dd5e7d1c4c4524cdcde2d7103774
GitFraize <zabrovsky2000denis@gmail.com>
2021-09-27 18:04:16 +0300

    Remove useless keys

--- Weather/Structures/Screens/UserDataRepository.swift
+++ Weather/Structures/Screens/UserDataRepository.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-private let savedCoordKeyExists = "WeatherApp_coordinates_exists"
-private let savedCityNameKeyExists = "WeatherApp_city_exists"
-
 private let cityKeyName = "WeatherApp_city_name"
 private let coordKeyLat = "WeatherApp_coordinates_latitude"
 private let coordKeyLon = "WeatherApp_coordinates_longtitude"
@@ -16,25 +13,24 @@ class UserDataRepository {
     }
     
     func getSavedCoordinates() -> Coord? {
-        guard userDefaults.bool(forKey: savedCoordKeyExists) else { return nil }
+        
+        guard isKeyExists(key: coordKeyLat) else { return nil }
         return Coord(
             lat: userDefaults.double(forKey: coordKeyLat),
             lon: userDefaults.double(forKey: coordKeyLon))
     }
     
     func getSavedCityName() -> String? {
-        guard userDefaults.bool(forKey: savedCityNameKeyExists) else { return nil }
+        guard isKeyExists(key: cityKeyName) else { return nil }
         return userDefaults.string(forKey: cityKeyName)
     }
     
     func saveCity(lat: Double, lon: Double) {
         userDefaults.set(lat, forKey: coordKeyLat)
         userDefaults.set(lon, forKey: coordKeyLon)
-        userDefaults.set(true, forKey: savedCoordKeyExists)
     }
     
     func saveCityName(name: String) {
         userDefaults.set(name, forKey: cityKeyName)
-        userDefaults.set(true, forKey: savedCityNameKeyExists)
     }
 }
