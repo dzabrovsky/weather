c12a16c3066518c4bafbeded150fd13021b7dadd
GitFraize <zabrovsky2000denis@gmail.com>
2021-09-27 16:13:24 +0300

    Replace UserDafaults.standart to needle userDefaults

--- Weather/Structures/Screens/UserDataManager.swift
+++ Weather/Structures/Screens/UserDataManager.swift
@@ -9,33 +9,36 @@ private let coordKeyLon = "WeatherApp_coordinates_longtitude"
 
 class UserDataManager {
     
-    static func getSavedCoordinates() -> Coord? {
+    private let userDefaults = UserDefaults.standard
+    
+    func getSavedCoordinates() -> Coord? {
         
-        if UserDefaults.standard.bool(forKey: savedCoordKeyExists) {
-            let lat = UserDefaults.standard.double(forKey: coordKeyLat)
-            let lon = UserDefaults.standard.double(forKey: coordKeyLon)
+        if userDefaults.bool(forKey: savedCoordKeyExists) {
+            let lat = userDefaults.double(forKey: coordKeyLat)
+            let lon = userDefaults.double(forKey: coordKeyLon)
             return Coord(lat: lat, lon: lon)
         }else{
             return nil
         }
     }
     
-    static func getSavedCityName() -> String? {
-        if UserDefaults.standard.bool(forKey: savedCityNameKeyExists) {
-            let name = UserDefaults.standard.string(forKey: cityKeyName)
+    func getSavedCityName() -> String? {
+        if userDefaults.bool(forKey: savedCityNameKeyExists) {
+            let name = userDefaults.string(forKey: cityKeyName)
             return name
         }else{
             return nil
         }
     }
     
-    static func saveCity(lat: Double, lon: Double) {
-        UserDefaults.standard.set(lat, forKey: coordKeyLat)
-        UserDefaults.standard.set(lon, forKey: coordKeyLon)
-        UserDefaults.standard.set(true, forKey: savedCoordKeyExists)
+    func saveCity(lat: Double, lon: Double) {
+        userDefaults.set(lat, forKey: coordKeyLat)
+        userDefaults.set(lon, forKey: coordKeyLon)
+        userDefaults.set(true, forKey: savedCoordKeyExists)
     }
-    static func saveCityName(name: String) {
-        UserDefaults.standard.set(name, forKey: cityKeyName)
-        UserDefaults.standard.set(true, forKey: savedCityNameKeyExists)
+    
+    func saveCityName(name: String) {
+        userDefaults.set(name, forKey: cityKeyName)
+        userDefaults.set(true, forKey: savedCityNameKeyExists)
     }
 }
