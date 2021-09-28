a4aa5a0af875885439d5b2a274eed5fb57206bc9
GitFraize <zabrovsky2000denis@gmail.com>
2021-09-28 15:25:58 +0300

    Add get current weather by location

--- Weather/Structures/Screens/Facades/AlamofireFacade.swift
+++ Weather/Structures/Screens/Facades/AlamofireFacade.swift
@@ -91,6 +91,38 @@ class AlamofireFacade {
             }
     }
     
+    func getCurrentWeather(lat: Double, lon: Double, completion: @escaping (CityListItem) -> ()) {
+        
+        let lang = UserDataRepository.shared.getLang()
+        let APIKey = UserDataRepository.shared.getAPIKey()
+        let units = UserDataRepository.shared.getUnits()
+        
+        let APIUrl = "https://api.openweathermap.org/data/2.5/weather"
+        
+        guard let url = URL(string: ("\(APIUrl)?lat=\(lat)&lon=\(lon)&appid=\(APIKey)&lang=\(lang)&units=\(units)").encodeUrl)
+        else { return }
+        var result = CityListItem()
+        
+        AF.request(url)
+            .validate()
+            .responseDecodable(of: Current.self) { (response) in
+                switch response.result {
+                case .success(let data):
+                    
+                    result.lat = data.coord.lat
+                    result.lon = data.coord.lon
+                    result.name = data.name
+                    result.temp = data.main.temp
+                    result.tempFeelsLike = data.main.feelsLike
+                    result.icon = data.weather[0].icon
+                    
+                    completion(result)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
     func searchCity(_ cityName: String, completion: @escaping (SearchGeoNames) -> ()){
         
         let lang = UserDataRepository.shared.getLang()
