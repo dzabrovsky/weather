cc04d5f596e132fef2f7735324e259e25c8ebdd5
GitFraize <zabrovsky2000denis@gmail.com>
2021-09-29 13:41:18 +0300

    Delete unused field

--- Weather/Structures/SearchGeoNames.swift
+++ Weather/Structures/SearchGeoNames.swift
@@ -5,13 +5,11 @@ struct SearchGeoNames: Codable {
 }
 
 struct SearchGeoName: Codable {
-    let population: Int?
     let name: String
     let lat: String
     let lon: String
 
     enum CodingKeys: String, CodingKey {
-        case population = "population"
         case name = "name"
         case lat = "lat"
         case lon = "lng"
