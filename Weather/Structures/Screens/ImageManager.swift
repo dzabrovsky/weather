0b6c018fd5f8bb8489d20d647ec3240fb3bab8f4
GitFraize <zabrovsky2000denis@gmail.com>
2021-09-29 12:48:22 +0300

    Extract getting image set by name to getImageSetByName

--- Weather/Structures/Screens/ImageManager.swift
+++ Weather/Structures/Screens/ImageManager.swift
@@ -9,82 +9,33 @@ import UIKit
 
 class ImageManager {
     
-    static func getIconAnimateByCode(_ code: String) -> [UIImage] {
-        switch code {
-        case "01d","01n":
-            return [#imageLiteral(resourceName: "01d")]
-        case "02d","02n":
-            var imageSet: [UIImage] = []
-            for i in 0...30 {
-                if i < 10 {
-                    if let image = UIImage.init(named: "Sun_120x120_0000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }else{
-                    if let image = UIImage.init(named: "Sun_120x120_000" + String(i)) {
-                        imageSet.append(image)
-                    }
+    private static func getImageSetByName(_ code: String) -> [UIImage] {
+        var imageSet: [UIImage] = []
+        for i in 0...30 {
+            if i < 10 {
+                if let image = UIImage.init(named: code + "_" + String(i)) {
+                    imageSet.append(image)
+                }
+            }else{
+                if let image = UIImage.init(named: code + "_" + String(i)) {
+                    imageSet.append(image)
                 }
             }
-            return imageSet
+        }
+        return imageSet
+    }
+    
+    static func getIconAnimateByCode(_ code: String) -> [UIImage] {
+        
+        switch code {
+        case "02d","02n", "04d","04n":
+            return getImageSetByName("02d")
         case "03d","03n":
             return [#imageLiteral(resourceName: "03d")]
-        case "04d","04n":
-            var imageSet: [UIImage] = []
-            for i in 0...30 {
-                if i < 10 {
-                    if let image = UIImage.init(named: "Sun_120x120_0000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }else{
-                    if let image = UIImage.init(named: "Sun_120x120_000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }
-            }
-            return imageSet
-        case "09d","09n":
-            var imageSet: [UIImage] = []
-            for i in 0...30 {
-                if i < 10 {
-                    if let image = UIImage.init(named: "Rain_120x120_0000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }else{
-                    if let image = UIImage.init(named: "Rain_120x120_000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }
-            }
-            return imageSet
-        case "10d","10n":
-            var imageSet: [UIImage] = []
-            for i in 0...30 {
-                if i < 10 {
-                    if let image = UIImage.init(named: "Rain_120x120_0000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }else{
-                    if let image = UIImage.init(named: "Rain_120x120_000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }
-            }
-            return imageSet
+        case "09d","09n", "10d","10n":
+            return getImageSetByName("09d")
         case "11d","11n":
-            var imageSet: [UIImage] = []
-            for i in 0...30 {
-                if i < 10 {
-                    if let image = UIImage.init(named: "Thunder_120x120_0000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }else{
-                    if let image = UIImage.init(named: "Thunder_120x120_000" + String(i)) {
-                        imageSet.append(image)
-                    }
-                }
-            }
-            return imageSet
+            return getImageSetByName("11d")
         default:
             return [#imageLiteral(resourceName: "01d")]
         }
