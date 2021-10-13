3317d3903117570d01c5c25e169207babef59118
GitFraize <zabrovsky2000denis@gmail.com>
2021-10-13 15:23:32 +0300

    Add constant screenScale

--- Weather/Constants.swift
+++ Weather/Constants.swift
@@ -1,4 +1,4 @@
-import Foundation
+import UIKit
 
 let langConstant: String = "ru"
 let unitsConstant: String = "metric"
@@ -6,3 +6,5 @@ let APIKeyConstant:String = "830e252225a6214c4370ecfee9b1d912"
 
 let defaultCityName = "Moscow"
 let defaultCoordinates = Coordindates(lat: 55.751244, lon: 37.618423)
+
+let screenScale: CGFloat = UIScreen.main.bounds.width / 375
