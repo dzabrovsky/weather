b8c7f1491a4750ff3a6d01d63cab3e292ab5de49
GitFraize <zabrovsky2000denis@gmail.com>
2021-10-13 15:34:29 +0300

    Refactory GeneralDayForecastAdapter

--- Weather/Structures/Screens/Adapters/GeneralDayForecastAdapter.swift
+++ Weather/Structures/Screens/Adapters/GeneralDayForecastAdapter.swift
@@ -9,6 +9,7 @@ class GeneralDayForecastAdapter: NSObject {
 }
 
 extension GeneralDayForecastAdapter: UITableViewDataSource {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -21,25 +22,15 @@ extension GeneralDayForecastAdapter: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let data = data else { return UITableViewCell() }
         if indexPath.row > 0{
-            
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UIWeatherDayCell", for: indexPath) as? UIWeatherDayCell else {
-                assertionFailure()
-                return UITableViewCell()
-            }
-            cell.setupCell()
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UIWeatherDayCell", for: indexPath) as? UIWeatherDayCell
+            else { return UITableViewCell() }
             cell.refresh(data.forecast[indexPath.row])
             return cell
-            
         }else{
-            
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UITodayWeatherCell", for: indexPath) as? UITodayWeatherCell else {
-                assertionFailure()
-                return UITableViewCell()
-            }
-            cell.setupCell()
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UITodayWeatherCell", for: indexPath) as? UITodayWeatherCell
+            else { return UITableViewCell() }
             cell.refresh(data.forecast[indexPath.row])
             return cell
-            
         }
     }
 }
