8598fb0e4ee8d9dde52ae6509d4c80d3166ae617
GitFraize <zabrovsky2000denis@gmail.com>
2021-10-18 15:08:28 +0300

    Add new global constants

--- Weather/Constants.swift
+++ Weather/Constants.swift
@@ -2,9 +2,16 @@ import UIKit
 
 let langConstant: String = "ru"
 let unitsConstant: String = "metric"
-let APIKeyConstant:String = "830e252225a6214c4370ecfee9b1d912"
+let APIKeyConstant: String = "830e252225a6214c4370ecfee9b1d912"
+let APIUserNameConstant: String = "ivan"
 
 let defaultCityName = "Moscow"
 let defaultCoordinates = Coordindates(lat: 55.751244, lon: 37.618423)
 
 let screenScale: CGFloat = UIScreen.main.bounds.width / 375
+
+let forecastURL = "https://api.openweathermap.org/data/2.5/forecast"
+let timemachineURL = "http://api.openweathermap.org/data/2.5/onecall/timemachine"
+let citiesJSONURL = "http://api.geonames.org/citiesJSON"
+let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather"
+let searchJSONURL = "http://api.geonames.org/searchJSON?"
